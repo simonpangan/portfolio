d8301e37f1c5a07bed486dc1be21e163b8fb9b42
Simon Pangan <simonjoseph.pangan@gmail.com>
2022-09-11 01:18:37 +0800

    add active link

--- src/fragments/NavIListtem.tsx
+++ src/fragments/NavIListtem.tsx
@@ -1,19 +1,23 @@
 import './NavIListtem.scss';
 import Route from '../interfaces/RouteInterface';
 
+import { NavLink } from "react-router-dom";
+
 type Props = {
     route: Route,
     className?: string
 };
 
-export default ({route, className = ''} : Props) => {
-    const { name, url } = route;
+export default ({route, className = ''} : Props) =>     {
+    const { id, name, url } = route;
 
     return (
-        <li key={name} className={`nav-item navbar-item-collapse ${className}`}>
-             <a className="nav-link" href="portfolio.html">
+        <li key={id} className={`nav-item navbar-item-collapse ${className}`}>
+            <NavLink to={url}  className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+            }>
                 {name}
-             </a>
+            </NavLink>
          </li>
      )
 };
