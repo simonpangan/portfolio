31e78ed782f3ea53a7b0f32f121e54285dea5bed
Simon Pangan <simonjoseph.pangan@gmail.com>
2022-09-11 20:50:46 +0800

    add contact header and update main contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,6 +1,18 @@
+import './Contact.css';
+
+import ContactForm from "./ContactForm";
+import ContactIntro from "./ContactIntro";
+import ContactHeader from "./ContactHeader";
+
 function Contact() {
     return (
-        <div>Contact</div>
+        <>
+            <ContactHeader />
+            <div className="contact mx-auto">
+                <ContactIntro />
+                <ContactForm />
+            </div>
+        </>
     );
 }
 
---
+++ src/pages/Contact/ContactHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageHeader from "../../components/PageHeader";
+
+function ContactHeader() {
+  return (
+      <PageHeader>
+          <h1>Contact</h1>
+      </PageHeader>
+  );
+}
+
+export default ContactHeader;
