3519dab0195753d67b6e11d5ed529c9a7109459a
simonpangan <simonjoseph.pangan@gmail.com>
2022-09-20 19:44:41 +0800

    update portfolio header

--- src/pages/Portfolio/PortfolioHeader.tsx
+++ src/pages/Portfolio/PortfolioHeader.tsx
@@ -3,12 +3,12 @@ import './PortfolioHeader.scss';
 import PageHeader from "../../components/Layout/PageHeader";
 
 function PortfolioHeader() {
-  return (
-      <PageHeader className='projects-header'>
-          <h1>Projects</h1>
-          <p><strong>In-depth Case Studies to show you what I can offer and how I work</strong></p>
-      </PageHeader>
-  );
+    return (
+        <PageHeader baseClass="projects-header">
+            <h1>Projects</h1>
+            <p><strong>In-depth Case Studies to show you what I can offer and how I work</strong></p>
+        </PageHeader>
+    );
 }
 
 export default PortfolioHeader;
